43f6925c35f40f7fbfca4d7a737c21c2d6fd87a3
QuinnS <sun00217@algonquinlive.com>
2024-10-22 23:15:43 -0400

    Initial commit

---
+++ app/src/main/java/com/example/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.lab2flag;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_linear); // 修改为相应的布局文件
+
+        Button button = findViewById(R.id.button);
+        Switch switch1 = findViewById(R.id.switch1);
+
+        button.setOnClickListener(v -> {
+            String message = getResources().getString(R.string.toast_message);
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+        });
+
+        switch1.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            String message = getResources().getString(R.string.switch_on_off) + (isChecked ? " on" : " off");
+            Snackbar snackbar = Snackbar.make(buttonView, message, Snackbar.LENGTH_LONG);
+            snackbar.setAction(getResources().getString(R.string.undo_action), v -> switch1.setChecked(!isChecked));
+            snackbar.show();
+        });
+    }
+}
